d2b6b82878a1ac440e219f524fce7434eb1654f9
Lorany <lorranyfdb@gmail.com>
2023-01-28 19:57:45 -0300

    Commit dos desafios - 01 operações booleanas e 02 variáveis

---
+++ desafio02.js
@@ -0,0 +1,12 @@
+//- Qual o seu nome?
+//- Quantos anos você tem?
+//- Qual linguagem de programação você está estudando?
+
+const nome = prompt ("Digite seu nome: ")
+const idade = prompt ("Digite sua idade: ")
+const linguagem = prompt ("Digite linguagem de programação você está estudando: ")
+
+const msg = `Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`
+
+alert (msg)
+
